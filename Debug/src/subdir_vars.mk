9434986de6bcc53f857f0d32f7d49ad3c5cdd30e
Gary Deco <gsdecosmo@outlook.com>
2020-10-25 18:23:34 -0400

    New structure: added src, and inc.

---
+++ Debug/src/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/test.c 
+
+C_DEPS += \
+./src/test.d 
+
+OBJS += \
+./src/test.obj 
+
+OBJS__QUOTED += \
+"src\test.obj" 
+
+C_DEPS__QUOTED += \
+"src\test.d" 
+
+C_SRCS__QUOTED += \
+"../src/test.c" 
+
+
